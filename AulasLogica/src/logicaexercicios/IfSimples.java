5652a03b2e4ac6c4001bf24213bc1f9eb35d9432
Prosopagnosatico <168598761+Prosopagnosatico@users.noreply.github.com>
2025-09-10 11:17:21 -0300

    Add files via upload

---
+++ AulasLogica/src/logicaexercicios/IfSimples.java
@@ -0,0 +1,13 @@
+//Exercicio 1
+
+package logicaexercicios;
+
+public class IfSimples {
+    public static void main(String[] args) {
+        int numero = 2;
+        if(numero < 0){
+            System.out.println("Esse numero é negativo");
+            else{
+        }
+    }
+}
